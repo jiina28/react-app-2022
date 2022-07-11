38e6b5f5e4148eb5915aad033fde53e84b342413
jiina28 <jiina_28@naver.com>
2022-07-11 11:13:46 +0900

    컴포넌트 만들기

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return <header>
+  <h1><a href="/index.html">WEB</a></h1>
+</header>
+}
+
+function Article() {
+  return <article>
+  <h2>Welcome</h2>
+  Hello, WEB!
+</article>
+}
+
+function Nav() {
+  return  <nav>
+  <ol>
+    <li><a href="/read/1.html">HTML</a></li>
+    <li><a href="/read/2.html">CSS</a></li>
+    <li><a href="/read/3.html">JavaScript</a></li>
+  </ol>
+</nav>
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Article />
+    </div>
+  );
+}
+
+export default App;
