bad268ed3073bd9a3c0f487a9e20ec176c2ccb70
jiina28 <jiina_28@naver.com>
2022-07-11 15:43:10 +0900

    state 추가

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function Header() {
+function Header(props) {
   return <header>
-  <h1><a href="/index.html">WEB - Login</a></h1>
-</header>
+    <h1><a href='/index.html' onClick={(event) => {
+      event.preventDefault();
+      props.onSelect();
+    }}>WEB - Login</a></h1>
+  </header>
 }
 
-function Article() {
+function Article(props) {
   return <article>
-  <h2>Welcome</h2>
-  Hello, WEB!
+    <h2>Welcome, {props.title}</h2>
+    {props.body}!
 </article>
 }
 
-function Nav() {
-  return  <nav>
-  <ol>
-    <li><a href="/read/1.html">HTML</a></li>
-    <li><a href="/read/2.html">CSS</a></li>
-    <li><a href="/read/3.html">JavaScript</a></li>
-  </ol>
-</nav>
+function Nav(props) {
+  const callback = e =>
+    <li key={e.id}>
+      <a href={'/read/' + e.id} onClick={(event) => {
+        event.preventDefault();
+        props.onSelect();
+      }}>{e.title}</a>
+    </li>
+
+  const tag = props.data.map(callback)
+
+  return <nav>
+    <ol>
+      {tag}
+    </ol>
+  </nav>
 }
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' }
+  ]
+
+  let content = null;
+  if(mode === 'WELCOME') {
+    content = <Article title='WELCOME' body='Hello, WEB'></Article>
+  }
+  else if(mode === 'READ') {
+    content = <Article title='READ' body='Hello, READ'></Article>
+  }
+
   return (
     <div>
-      <Header />
-      <Nav />
-      <Article />
+      <Header onSelect={() => setMode('WELCOME')}></Header>
+      <Nav data={topics} onSelect={() => setMode('READ')}></Nav>
+      {content}
     </div>
   );
 }
