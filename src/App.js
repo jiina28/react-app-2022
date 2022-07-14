95395d6ec6b4e7d7fc144e557ad227cd09911a21
jiina28 <jiina_28@naver.com>
2022-07-14 11:38:29 +0900

    create 버튼

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
-import {Link, Routes, Route, useParams} from 'react-router-dom';
+import {Link, Routes, Route, useParams, useNavigate} from 'react-router-dom';
 
 function Header(props) {
   return <header>
@@ -39,10 +39,15 @@ function Read(props) {
   return <Article title={topic.title} body={'Hello, '+topic.body}></Article>
 }
 
-function Create() {
+function Create(props) {
   return <article>
-    <h1>create</h1>
-    <form action="/api/create">
+    <h1>create</h1> 
+    <form action="/api/create" onSubmit={evt => {
+      evt.preventDefault();
+      const title = evt.target.title.value;
+      const body = evt.target.body.value;
+      props.onCreate(title, body);
+    }}>
       <p><input type="text" name="title" placeholder="제목" /></p>
       <p><textarea name="body" placeholder="본문"></textarea></p>
       <p><input type="submit" value="생성"></input></p>
@@ -51,12 +56,14 @@ function Create() {
 }
 
 function App() {
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
     { id: 2, title: 'css', body: 'css is ...' },
     { id: 3, title: 'javascript', body: 'javascript is ...' }
-  ]
-
+  ]);
+  const navigate = useNavigate();
+  
   return (
     <div>
       <Header></Header>
@@ -65,7 +72,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Article title='WELCOME' body='Hello, WEB'></Article>}></Route>
         <Route path="/read/:id" element={<Read data={topics}></Read>}></Route>
-        <Route path="/create" element={<Create></Create>} ></Route>
+        <Route path="/create" element={<Create onCreate={(title, body) => {
+          // Update topics          
+          const newTopics = [...topics];
+          newTopics.push({id:nextId, title:title, body:body});
+          setTopics(newTopics);
+
+          // topics.push({id:nextId, title:title, body:body});
+          // setTopics(topics);
+
+          navigate('/read/' + nextId);  // 자동으로 페이지 이동
+          setNextId(nextId + 1);
+        }}></Create>} ></Route>
       </Routes>
       <Link to="/create">create</Link>
     </div>
